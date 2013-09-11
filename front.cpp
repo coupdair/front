f1c6cb4dfe6c550e0870585e942c94ff6279b690
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2013-09-11 14:46:43 +0200

    setup binarisation. modified:   front.cpp: add both opton and function call for binarisation.

--- front.cpp
+++ front.cpp
@@ -6,6 +6,18 @@
 #include "CImg.h"
 using namespace cimg_library;
 
+//! either display or/and print based on display at compilation or running time.
+template<typename T>
+void display_print(CImg<T> &img,bool show,std::string &title)
+{
+#if cimg_display!=0
+  if(show) img.display(title.c_str());
+  else img.print(title.c_str());
+#else
+  img.print(title.c_str());
+#endif
+}//display_print
+
 int main(int argc,char **argv)
 {
   //option{
@@ -16,7 +28,8 @@ usage: ./front -h -I\n \
        ./front -t 0.3 -i image_xt.cimg -o graph_xpVSt.cimg\n \
 version: "+std::string(VERSION)+"\n compilation date: " \
             ).c_str());//cimg_usage
-  std::string input_file_name=cimg_option("-i","data.cimg","Input data.");
+  std::string  input_file_name=cimg_option("-i","data.cimg" ,"Input  data.");
+  std::string output_file_name=cimg_option("-o","graph.cimg","Output data.");
   ///standard GNU/CImg program options
   const bool show_h   =cimg_option("-h",    false,NULL);//-h hidden option
         bool show_help=cimg_option("--help",show_h,"help (or -h option)");show_help=show_h|show_help;//same --help or -h option
@@ -26,18 +39,18 @@ version: "+std::string(VERSION)+"\n compilation date: " \
   const bool showX=cimg_option("-X",false,NULL);//-X hidden option
   bool show=cimg_option("--show",show_h,"show GUI (or -X option)");show=showX|show;//same --show or -X option
 #endif
+  ///threshold
+  float binary_threshold=cimg_option("-t",0.3,"binary threshold for input image (e.g. 0.3)");
   ///stop help request
   if(show_help) {/*print_help(std::cerr);*/return 0;}
   //}option
 
   CImg<float> img_src(input_file_name.c_str());
-  
-#if cimg_display!=0
-  if(show) img_src.display(input_file_name.c_str());
-  else img_src.print(input_file_name.c_str());
-#else
-  img_src.print(input_file_name.c_str());
-#endif
+  display_print(img_src,show,input_file_name);
+
+  CImg<int> img_bin=img_src.get_threshold(binary_threshold);
+  display_print(img_bin,show,output_file_name);
+img_bin.save(output_file_name.c_str());
 
   std::exit(0);
   return 0;
