8bb0109a6f409d87aad169df75d65d60de559308
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2013-09-11 14:24:45 +0200

    add program version. new file:   _info.txt: compilation and run lines. modified:   front.cpp: add VERSION for program.

--- front.cpp
+++ front.cpp
@@ -10,23 +10,25 @@ int main(int argc,char **argv)
 {
   //option{
   ///usage
-  cimg_usage("front detection program of the Laboratoire de PhysicoChimie des Processus de Combustion et de l'Atmosphere (PC2A), \
+  cimg_usage(std::string("front position detection program of the Laboratoire de PhysicoChimie des Processus de Combustion et de l'Atmosphere (PC2A), \
 it uses different GNU libraries (see --info option)\n\n \
 usage: ./front -h -I\n \
        ./front -t 0.3 -i image.nc -o graph.cimg\n \
-\ncompilation date: "
-            );//cimg_usage
+version: "+std::string(VERSION)+"\n compilation date: " \
+            ).c_str());//cimg_usage
   std::string input_file_name=cimg_option("-i","data.cimg","Input data.");
+  ///standard GNU/CImg program options
   const bool show_h   =cimg_option("-h",    false,NULL);//-h hidden option
         bool show_help=cimg_option("--help",show_h,"help (or -h option)");show_help=show_h|show_help;//same --help or -h option
   bool show_info=cimg_option("-I",false,NULL);//-I hidden option
   if( cimg_option("--info",show_info,"show compilation options (or -I option)") ) {show_info=true;cimg_library::cimg::info();}//same --info or -I option
-  //}option
+  ///stop help request
   if(show_help) {/*print_help(std::cerr);*/return 0;}
+  //}option
 
   CImg<float> img_src(input_file_name.c_str());
   img_src.display(input_file_name.c_str());
   std::exit(0);
   return 0;
-}
+}//main
 
