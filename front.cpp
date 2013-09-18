e194499d76d4017ed8c7a86a024d13d6d8984132
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2013-09-18 16:09:44 +0200

    setup mean profile gradient g=f(t)/f(t+1)

--- front.cpp
+++ front.cpp
@@ -53,7 +53,7 @@ version: "+std::string(VERSION)+"\n compilation date: " \
   display_print(img_src,show,input_file_name);
 
   ///
-  CImg<float> profile(img_src.depth(),1,1,4);
+  CImg<float> profile(img_src.depth(),1,1,5);
   cimg_forZ(img_src,t)
   {
     //get single image
@@ -64,10 +64,14 @@ version: "+std::string(VERSION)+"\n compilation date: " \
     profile(t,0,0,2)=img.mean();//mean
   }//time
   //save
-  profile.display_graph("min/max/mean v.s. time");
-  ///ratio
-  cimg_for_inZ(img_src,0,profile.width()-2,t) profile(t+1,0,0,3)=(profile(t+1,0,0,1)==0)?0:profile(t,0,0,1)/profile(t+1,0,0,1);
-  (profile.get_shared_channel(3)).display_graph("max(t)/max(t+1) v.s. time");
+  profile.display_graph("min,max,mean v.s. time");
+  ///grad
+  cimg_for_inZ(img_src,0,profile.width()-2,t)
+  {
+    profile(t+1,0,0,3)=(profile(t+1,0,0,1)==0)?0:profile(t,0,0,1)/profile(t+1,0,0,1);
+    profile(t+1,0,0,4)=(profile(t+1,0,0,2)==0)?0:profile(t,0,0,2)/profile(t+1,0,0,2);
+  }
+  (profile.get_shared_channels(3,4)).display_graph("max,mean gradients v.s. time e.g. g=f(t)/f(t+1)");
 
 /*
   ///binarisation
